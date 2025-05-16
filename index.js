52e7dd1b975c47acdb0d8d229bcf467015409502
kiwicabs2026 <kiwi.cabs@xtra.co.nz>
2025-05-16 15:11:02 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 const express = require('express');
-const { google } = require('googleapis');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,33 +10,9 @@ app.post('/new-booking', async (req, res) => {
   const booking = req.body;
   console.log('Received booking:', booking);
 
-  const auth = new google.auth.GoogleAuth({
-    credentials: require(process.env.GOOGLE_APPLICATION_CREDENTIALS),
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-  });
+   res.send({ status: 'Booking received successfully' });
 
-  const sheets = google.sheets({ version: 'v4', auth: await auth.getClient() });
-
-  const spreadsheetId = '1oig42kp7Kd56dajZ8bsyFrXG9sPJCOHePLZ6WI7SFBQ';
-
-  const sheetName = 'Sheet1';
-
-  await sheets.spreadsheets.values.append({
-    spreadsheetId,
-    range: `${sheetName}!A:E`,
-    valueInputOption: 'USER_ENTERED',
-    requestBody: {
-      values: [[
-        new Date().toISOString(),
-        booking.name || '',
-        booking.address || '',
-        booking.pickup_time || '',
-        booking.caller || ''
-      ]]
-    }
-  });
-
-  res.send({ status: 'added to sheet' });
+  
 });
 
 app.listen(PORT, () => {
